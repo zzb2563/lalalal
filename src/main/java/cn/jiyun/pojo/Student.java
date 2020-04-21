94b8caa0a9f76486addfbecb3a185d6c4f01b576
zhao.xuepu <zhao.xuepu@163.com>
2020-04-21 22:10:42 +0800

    111

---
+++ src/main/java/cn/jiyun/pojo/Student.java
@@ -0,0 +1,18 @@
+package cn.jiyun.pojo;
+
+public class Student {
+
+    private String userName;
+
+    // alt + insert  快捷键  处理 get  set方法
+    // alt +enter  快捷键  智能提示
+
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
