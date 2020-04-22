8e25ddab9c763f8242ae147c4ced6150b09092de
zzb2563 <64066631+zzb2563@users.noreply.github.com>
2020-04-22 17:19:16 +0800

    Update Student.java

--- src/main/java/cn/jiyun/pojo/Student.java
+++ src/main/java/cn/jiyun/pojo/Student.java
@@ -3,6 +3,7 @@ package cn.jiyun.pojo;
 public class Student {
 
     private String userName;
+    private String sex;
 
     // alt + insert  快捷键  处理 get  set方法
     // alt +enter  快捷键  智能提示
@@ -14,5 +15,11 @@ public class Student {
 
     public void setUserName(String userName) {
         this.userName = userName;
+    }
+    public String getSex(){
+     return sex;
+    }
+      public void setSex(String sex) {
+        this.usexe = sex;
     }
 }
